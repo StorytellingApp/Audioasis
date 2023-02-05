0cfd684716a43dfa88435fc9b24932e9256cea17
Tstrand123 <tstrandg@uci.edu>
2023-02-04 17:37:24 -0800

    Upload File Added

--- lib/upload_pages.dart
+++ lib/upload_pages.dart
@@ -1,10 +1,14 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'auth_page.dart';
 import 'utils.dart';
 import 'verify_email_page.dart';
 import 'package:file_picker/file_picker.dart';
+import 'dart:io';
 
 //https://www.youtube.com/watch?v=Jt3OSCe7eOw
 //https://pub.dev/packages/file_picker
@@ -17,9 +21,29 @@ class UploadAudio extends StatefulWidget {
 }
 
 class _UploadAudioState extends State<UploadAudio> {
+  PlatformFile? pickedFile;
+  UploadTask? uploadTask;
+
+  Future uploadFile() async {
+    final path = 'Audio/${pickedFile!.name}'; //TODO: Unique name
+    final file = File(pickedFile!.path!);
+
+    final ref = FirebaseStorage.instance.ref().child(path);
+    ref.putFile(file);
+
+    final snapshot = await uploadTask!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+    //TODO: Progress bar?
+    //TODO: add auto route or something after upload?
+  }
+
   Future selectFile() async{
-    //pick up at 3:24
-    final result = await FilePicker.platform.pickFiles();
+    final result = await FilePicker.platform.pickFiles(type: FileType.audio);
+    if (result == null) return;
+
+    setState(() {
+      pickedFile = result.files.first;
+    });
   }
 
 
@@ -33,13 +57,16 @@ class _UploadAudioState extends State<UploadAudio> {
         child: Center(
           child: Column(
             children: [
+              const SizedBox(height: 10,),
+              if (pickedFile != null) Text(pickedFile!.name),
+              const SizedBox(height: 25,),
               TextButton(
                 onPressed: selectFile,
                 child: const Text('Select File'),
               ),
               const SizedBox(height: 25,),
               TextButton(
-                onPressed: () => {},
+                onPressed: uploadFile,
                 child: const Text('Upload File'),
               ),
             ],
