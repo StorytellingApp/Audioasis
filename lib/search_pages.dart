9f98a46d2c9d4a785534468c09b2ff32c0e0937a
Tstrand123 <tstrandg@uci.edu>
2023-02-15 10:28:38 -0800

    Temp search page

--- lib/search_pages.dart
+++ lib/search_pages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,8 @@ import 'utils.dart';
 import 'verify_email_page.dart';
 import 'package:file_picker/file_picker.dart';
 import 'dart:io';
+import 'userClasses.dart';
+
 
 class SearchTabPage extends StatefulWidget {
   const SearchTabPage({Key? key}) : super(key: key);
@@ -18,6 +21,13 @@ class SearchTabPage extends StatefulWidget {
 class _SearchTabPageState extends State<SearchTabPage> {
   final searchController = TextEditingController();
   final searchKey = GlobalKey<FormState>();
+
+  String searchTerm = '';
+  Stream<List<Story>> readStory = FirebaseFirestore.instance.collection('Stories')
+      .where('storyName'.toLowerCase().trim(),isNotEqualTo: '')
+      .snapshots()
+      .map((snapshot) =>
+      snapshot.docs.map((doc) => Story.fromJson(doc.data())).toList());
 
   @override
   void dispose() {
@@ -40,6 +50,18 @@ class _SearchTabPageState extends State<SearchTabPage> {
         ));
   }
 
+  Stream<List<Story>> readStories() => FirebaseFirestore.instance.collection('Stories')
+      .where('storyName'.toLowerCase().trim(),isNotEqualTo: searchTerm.toLowerCase().trim())
+      .snapshots()
+      .map((snapshot) =>
+      snapshot.docs.map((doc) => Story.fromJson(doc.data())).toList());
+
+  Widget buildStories(Story story) => ListTile(
+    leading: const Icon(Icons.book),
+    title: Text(story.storyName),
+    subtitle: Text(story.description),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,16 +76,44 @@ class _SearchTabPageState extends State<SearchTabPage> {
             children: [
               Container(
                 padding: const EdgeInsets.all(16),
-                child: TextFormField(
+                child: TextField(
                   controller: searchController,
                   decoration: const InputDecoration(labelText: 'Search'),
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) => value != null && value.isEmpty ? 'Enter a Search Term' : null,
+                  onChanged: (value) {
+                    setState(() {
+                      searchTerm = searchController.text.trim();
+                      readStory = FirebaseFirestore.instance.collection('Stories')
+                          .where('storyName'.toLowerCase().trim(),isNotEqualTo: searchTerm.toLowerCase().trim())
+                          .snapshots()
+                          .map((snapshot) =>
+                          snapshot.docs.map((doc) => Story.fromJson(doc.data())).toList());
+                    });
+                  }
                 ),
               ),
-              ElevatedButton(
-                  onPressed: searchQuery,
-                  child: const Text('Search'))
+              //ElevatedButton(
+                  //onPressed: searchQuery,
+                  //child: const Text('Search'))
+              //Text(searchTerm), Has access to the changes
+              StreamBuilder(
+                stream: readStory,
+                builder: (context,snapshot) {
+                  if (snapshot.hasError){
+                    return const Text('Something went wrong');
+                  }else if (snapshot.hasData){
+                    final stories = snapshot.data!;
+
+                    return ListView(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      children: stories.map(buildStories).toList(),
+                    );
+                  }else{
+                    return const Center(child: CircularProgressIndicator(),);
+                  }
+                },
+              )
+
             ],
           ),
         ),
@@ -83,11 +133,50 @@ class SearchResults extends StatefulWidget {
 }
 
 class _SearchResultsState extends State<SearchResults> {
+  Stream<List<Story>> readStories() => FirebaseFirestore.instance.collection('Stories')
+      .where('storyName'.toLowerCase(),isEqualTo: widget.searchQuery.toLowerCase().trim())
+      .snapshots()
+      .map((snapshot) =>
+      snapshot.docs.map((doc) => Story.fromJson(doc.data())).toList());
+
+  Widget buildStories(Story story) => ListTile(
+    leading: const Icon(Icons.book),
+    title: Text(story.storyName),
+  );
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text(widget.searchQuery),
+      appBar: AppBar(), //Text(widget.searchQuery)
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Results for ${widget.searchQuery}'),
+              StreamBuilder(
+                stream: readStories(),
+                builder: (context,snapshot) {
+                  if (snapshot.hasError){
+                    return const Text('Something went wrong');
+                  }else if (snapshot.hasData){
+                    final stories = snapshot.data!;
+
+                    return ListView(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      children: stories.map(buildStories).toList(),
+                    );
+                  }else{
+                    return const Center(child: CircularProgressIndicator(),);
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
