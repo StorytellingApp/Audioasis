e25ac5abdcafa50b9c7eaddc5946c039448e75b6
Alma Flores <floreso2@uci.edu>
2023-02-21 22:01:25 -0800

    Added scrollable playlists, tried adding color.

--- lib/library_pages.dart
+++ lib/library_pages.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -19,16 +21,148 @@ class _LibraryTabPageState extends State<LibraryTabPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Library'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        alignment: Alignment.topCenter ,
         children: [
-          ElevatedButton(onPressed: () => FirebaseAuth.instance.signOut(),
-              child: const Text('Sign Out'))
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.blueGrey.withOpacity(0.8),
+                  Colors.white.withOpacity(0.0),
+                ],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                            "My Library",
+                          style: TextStyle(fontSize: 25, color: Colors.white),
+                        ),
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              radius: 20.0,
+                              backgroundImage: AssetImage('images/UserProfilePic.jpg'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    padding: EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        PlaylistCard(image: AssetImage('images/NoImageDefault.jpg'), title: "Liked"),
+                        SizedBox(width: 8),
+                        PlaylistCard(image: AssetImage('images/NoImageDefault.jpg'), title: "Listen Later"),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 32),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                            "Personal Playlists",
+                            style: TextStyle(fontSize: 25, color: Colors.white),
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            PlaylistsCard(image: AssetImage('images/NoImageDefault.jpg'), title: "Playlist"),
+                            SizedBox(width: 16),
+                            PlaylistsCard(image: AssetImage('images/NoImageDefault.jpg'), title: "Playlist"),
+                          ],
+                        ),
+                        SizedBox(height: 16)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
+      )
+    );
+  }
+}
+
+
+class PlaylistCard extends StatelessWidget {
+  final ImageProvider image;
+  final String title;
+  const PlaylistCard({Key? key, required this.image, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(image: image,
+            width: 240,
+            height: 125,
+            fit: BoxFit.fitHeight,
+        ),
+        Text(title),
+      ],
+    );
+  }
+}
+
+class PlaylistsCard extends StatelessWidget {
+  final AssetImage image;
+  final String title;
+  const PlaylistsCard({Key? key, required this.image, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        //clipBehavior: Clip.antiAlias,
+        child: Row(
+          children: [
+            Image(
+              image: image,
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: 8),
+            Text(title),
+          ],
+        ),
       ),
     );
   }
 }
+
+//ElevatedButton(onPressed: () => FirebaseAuth.instance.signOut(),
+//child: const Text('Sign Out'))
+
