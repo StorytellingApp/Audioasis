023a9d21eb7a36257915e1c0fd61257fe9abc1f3
Annahi Paez <aspaez@uci.edu>
2023-02-23 18:16:32 -0800

    updated hompage recently played card

--- lib/main_home_page.dart
+++ lib/main_home_page.dart
@@ -59,120 +59,149 @@ class _HomeTabPageState extends State<HomeTabPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Home',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.normal
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Home',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal
+                  ),
                 ),
-              ),
-              //TODO fix spacing between notification icon and profile also have to make profile clickable
-              IconButton( //notficiations
-                onPressed: () {},
-                icon: Icon(Icons.notifications, color: Colors.black,),
-              ),
-              const CircleAvatar( //profile
-                radius: 20.0,
-                backgroundImage: AssetImage('images/UserProfilePic.jpg'),
-              ),
-            ],
-          ), //home title
-          const SizedBox(
-            height: 15.0,
-          ),
-          const Text(
-            "Recently Played",
-            style: TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
+                //TODO fix spacing between notification icon and profile also have to make profile clickable
+                IconButton( //notifications
+                  onPressed: () {},
+                  icon: const Icon(Icons.notifications, color: Colors.black,),
+                ),
+                const CircleAvatar( //profile
+                  radius: 20.0,
+                  backgroundImage: AssetImage('images/UserProfilePic.jpg'),
+                ),
+              ],
+            ), //home title
+            const SizedBox(
+              height: 15.0,
             ),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          SizedBox(
-            height: 160,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (
-                      (context, index) {
-                    return Column(
-                      children: [
-                        Container( //cards for stories
-                          height: 125,
-                          width: 240,
-                          margin: const EdgeInsets.symmetric(horizontal: 8),
-                          decoration: BoxDecoration( //create box for story card
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(15),
-                            // image: DecorationImage(
-                            // image: AssetImage((['image']),
-                            // ),
-                            // fit: BoxFit.cover
+            const Text(
+              "Recently Played",
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            SizedBox( // creating the recently played card section of with the stories
+              height: 185,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (
+                        (context, index) {
+                      return Column(
+                        children: [
+                          Container( //cards for stories
+                            height: 125,
+                            width: 240,
+                            margin: const EdgeInsets.symmetric(horizontal: 8),
+                            decoration: BoxDecoration( //create box for story card
+                              borderRadius: BorderRadius.circular(15),
+                              image: const DecorationImage(
+                              image: AssetImage(
+                                ('images/UserProfilePic.jpg'),
+                              ),
+                                  fit: BoxFit.cover
+                            ),
                           ),
-                        ),
-
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        const Text(
-                          'Title',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 11,
-                              fontWeight: FontWeight.normal
                           ),
-                        ),
-                      ], //children
-                    );
-                  }),
+                            const SizedBox(
+                            height: 9,
+                          ),
+                          const Text(//under card image title of story text
+                            'Title',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal
+                            ),
+                          ),
+                          const Text(//under card image author text
+                            'Author',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal
+                            ),
+                          ),
+                        ], //children
+                      );
+                    }),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 15.0,
-          ),
-          const Text(
-            "Recommendations",
-            style: TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
+            const SizedBox(
+              height: 15.0,
             ),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          const Text(
-            "Top Picks",
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
+            const Text(
+              "Recommendations",
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
             ),
-          ),
-          SizedBox(
+            const SizedBox(
+              height: 24,
+            ),
+            const Text(
+              "Top Picks",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
               height: 195.0,
               child: ListView.separated(
-              padding: const EdgeInsets.all(16),
-              scrollDirection: Axis.horizontal,
-              itemCount: 4,
-              separatorBuilder: (context, _) => const SizedBox(width: 12,),
-              itemBuilder: (BuildContext context, int index) =>  buildCard(item: items[index], ),
-           ),
-          ),
+                padding: const EdgeInsets.all(16),
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                separatorBuilder: (context, _) => const SizedBox(width: 12,),
+                itemBuilder: (BuildContext context, int index) => buildCard(item: items[index], ),
+             ),
+            ),
+            const Text(
+              "New Releases for You",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 195.0,
+              child: ListView.separated(
+                padding: const EdgeInsets.all(16),
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                separatorBuilder: (context, _) => const SizedBox(width: 12,),
+                itemBuilder: (BuildContext context, int index) => buildCard(item: items[index], ),
+              ),
+            ),
+          ],
 
-        ],
+        ),
       ),
     );
   }
