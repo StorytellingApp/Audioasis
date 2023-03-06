0a19ba3a6920e83342affd7d5d4b8b6a2a8ff6cb
Tstrand123 <tstrandg@uci.edu>
2023-03-05 16:25:53 -0800

    Story Description Page Done

--- lib/storyScaffold.dart
+++ lib/storyScaffold.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/rendering.dart';
 import 'auth_page.dart';
 import 'utils.dart';
 import 'verify_email_page.dart';
@@ -102,8 +103,50 @@ class _DescriptionStoryPageState extends State<DescriptionStoryPage> {
               if (authorInfo.hasError || !authorInfo.hasData) {
                 return const Center(child: Text('An Error Occured'),);
               }else {
+                //Text(authorInfo.data!.userID)
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 15,),
+                    Center(child: Image.network(widget.art, width: 250,),),
+                    const SizedBox(height: 15,),
+                    //TODO: adjust row for things
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                      Image.network(authorInfo.data!.imageURL, width: 50,),
+                      Text(authorInfo.data!.firstName,style: const TextStyle(fontSize: 20, overflow: TextOverflow.ellipsis),),
+                        IconButton(onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => const PlayAudioStoryPage()));
+                        },
+                            icon: const Icon(Icons.play_circle_fill_outlined))
+                    ],
+                    ),
+                    const SizedBox(height: 15,),
+                    SizedBox(width: 250,
+                    child: Text(widget.description, style: const TextStyle(
+                      fontSize: 18,
+                    ),),),
+                    const SizedBox(height: 30,),
+                    const Text('Tags'),
+                    const SizedBox(height: 10,),
 
-                return Text(authorInfo.data!.userID);
+                    Container(
+                      margin: const EdgeInsets.all(16),
+                      height: 80,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: widget.tags.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        if (index == widget.tags.length - 1){
+                          return Text('${widget.tags[index]}');
+                        }
+                        return Text('${widget.tags[index]},     ');
+                      },
+                    ),
+                    ),
+
+                  ],
+                );
               }
             }
           },
@@ -128,3 +171,29 @@ class _DescriptionStoryPageState extends State<DescriptionStoryPage> {
     return tempUser;
   }
 }
+
+class PlayAudioStoryPage extends StatefulWidget {
+  const PlayAudioStoryPage({Key? key}) : super(key: key);
+
+  @override
+  State<PlayAudioStoryPage> createState() => _PlayAudioStoryPageState();
+}
+
+class _PlayAudioStoryPageState extends State<PlayAudioStoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Play Story'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
