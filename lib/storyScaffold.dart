7c89bd14ae5cf21d235f055dea00e4d1ddbc814a
Tstrand123 <tstrandg@uci.edu>
2023-03-06 18:01:08 -0800

    Finished Audio Player

--- lib/storyScaffold.dart
+++ lib/storyScaffold.dart
@@ -1,16 +1,11 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:destudio_test/searchCards.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/rendering.dart';
-import 'auth_page.dart';
-import 'utils.dart';
-import 'verify_email_page.dart';
-import 'package:file_picker/file_picker.dart';
-import 'dart:io';
 import 'userClasses.dart';
+
+//https://www.youtube.com/watch?v=MB3YGQ-O1lk
 
 class PlayStoryPage extends StatefulWidget {
   final String art;
@@ -108,6 +103,8 @@ class _DescriptionStoryPageState extends State<DescriptionStoryPage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const SizedBox(height: 15,),
+                    Text(widget.storyName),
+                    const SizedBox(height: 15,),
                     Center(child: Image.network(widget.art, width: 250,),),
                     const SizedBox(height: 15,),
                     //TODO: adjust row for things
@@ -116,7 +113,21 @@ class _DescriptionStoryPageState extends State<DescriptionStoryPage> {
                       Image.network(authorInfo.data!.imageURL, width: 50,),
                       Text(authorInfo.data!.firstName,style: const TextStyle(fontSize: 20, overflow: TextOverflow.ellipsis),),
                         IconButton(onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const PlayAudioStoryPage()));
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => PlayAudioStoryPage(
+                            art: widget.art,
+                            authorID: widget.authorID,
+                            description: widget.description,
+                            downloadURL: widget.downloadURL,
+                            series: widget.series,
+                            seriesID: widget.seriesID,
+                            storyID: widget.storyID,
+                            storyName: widget.storyName,
+                            tags: widget.tags,
+                            firstName: authorInfo.data!.firstName,
+                            imageURL: authorInfo.data!.imageURL,
+                            lastName: authorInfo.data!.lastName,
+                            userID: authorInfo.data!.userID,
+                          )));
                         },
                             icon: const Icon(Icons.play_circle_fill_outlined))
                     ],
@@ -173,13 +184,99 @@ class _DescriptionStoryPageState extends State<DescriptionStoryPage> {
 }
 
 class PlayAudioStoryPage extends StatefulWidget {
-  const PlayAudioStoryPage({Key? key}) : super(key: key);
+  final String art;
+  final String authorID;
+  final String description;
+  final String downloadURL;
+  final String series;
+  final String seriesID;
+  final String storyID;
+  final String storyName;
+  final List<dynamic> tags;
+
+  final String firstName;
+  final String imageURL;
+  final String lastName;
+  final String userID;
+
+
+  const PlayAudioStoryPage({Key? key,
+    required this.art,
+    required this.authorID,
+    required this.description,
+    required this.downloadURL,
+    required this.series,
+    required this.seriesID,
+    required this.storyID,
+    required this.storyName,
+    required this.tags,
+    required this.firstName,
+    required this.imageURL,
+    required this.lastName,
+    required this.userID,
+  }) : super(key: key);
+  //TODO: pass in all information via parameters
 
   @override
   State<PlayAudioStoryPage> createState() => _PlayAudioStoryPageState();
 }
 
 class _PlayAudioStoryPageState extends State<PlayAudioStoryPage> {
+  final audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setAudio();
+
+    if (mounted){
+      audioPlayer.onPlayerStateChanged.listen((state) {
+        if (mounted) {
+          setState(() {
+            isPlaying = state == PlayerState.playing;
+          });
+        }
+      });
+    }
+
+
+    if (mounted){
+      audioPlayer.onDurationChanged.listen((newDuration) {
+        if (mounted) {
+          setState(() {
+            duration = newDuration;
+          });
+        }
+
+      });
+    }
+
+
+    if (mounted){
+      audioPlayer.onPositionChanged.listen((newPosition) {
+        if (mounted) {
+          setState(() {
+            position = newPosition;
+          });
+        }
+      });
+    }
+
+  }
+
+
+  @override
+  dispose() {
+    audioPlayer.dispose();
+
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -189,11 +286,77 @@ class _PlayAudioStoryPageState extends State<PlayAudioStoryPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-
+            const SizedBox(height: 15,),
+            Center(child: Image.network(widget.art, width: 250,)),
+            const SizedBox(height: 15,),
+            Text(widget.storyName),
+            const SizedBox(height: 15,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.network(widget.imageURL,width: 75,),
+                Text(widget.firstName),
+              ],
+            ),
+            const SizedBox(height: 25,),
+            Slider(
+              min: 0,
+              max: duration.inSeconds.toDouble(),
+                value: position.inSeconds.toDouble(),
+                onChanged: (value) async {
+                  final position = Duration(seconds: value.toInt());
+                  await audioPlayer.seek(position);
+                },
+            ),
+            Padding(padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(formatTime(position)),
+                Text(formatTime(duration)),
+              ],
+            ),
+            ),
+            CircleAvatar(
+              radius: 35,
+              child: IconButton(
+                icon: Icon(
+                  isPlaying ? Icons.pause : Icons.play_arrow,
+                ),
+                iconSize: 50,
+                onPressed: () async {
+                  if (isPlaying) {
+                    await audioPlayer.pause();
+                  } else {
+                    await audioPlayer.resume();
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  String formatTime(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2,'0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+    return [
+      if (duration.inHours > 0) hours,
+      minutes,
+      seconds,
+    ].join(':');
+  }
+
+  Future setAudio() async {
+    audioPlayer.setReleaseMode(ReleaseMode.release);
+
+    audioPlayer.setSourceUrl(widget.downloadURL);
+
   }
 }
 
