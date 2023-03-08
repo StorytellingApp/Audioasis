312dcb7ae30f190343708a32a337ed716857b193
Tstrand123 <tstrandg@uci.edu>
2023-03-07 16:54:48 -0800

    Added tapping for genre cards

--- lib/search_pages.dart
+++ lib/search_pages.dart
@@ -14,7 +14,6 @@ import 'userClasses.dart';
 
 //https://www.youtube.com/watch?v=WbXTl9tiziI
 
-
 class SearchTabPage extends StatefulWidget {
   const SearchTabPage({Key? key}) : super(key: key);
 
@@ -38,106 +37,117 @@ class _SearchTabPageState extends State<SearchTabPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children:  const [
-                  Text(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
                   'Search',
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.normal
-                  ),
-                  ),
-                  ], //children
+                      fontStyle: FontStyle.normal),
                 ),
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  child: TextField(
-                    controller: searchController,
-                    decoration: const InputDecoration(
-                        labelText: 'Search for Authors, Stories, Playlist', prefixIcon: Icon(Icons.search)),
-                    onChanged: (val) {
-                      setState(() {
-                        storyName = val;
-                      });
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 200,
-                  child: StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                        .collection('Stories')
-                        .snapshots(),
-                    builder: (context, snapshots) {
-                      return (snapshots.connectionState ==
-                              ConnectionState.waiting)
-                          ? const Center(
-                              child: CircularProgressIndicator(),
-                            )
-                          : ListView.builder(
-                              itemCount: snapshots.data!.docs.length,
-                              itemBuilder: (context, index) {
-                                var data = snapshots.data!.docs[index].data()
-                                    as Map<String, dynamic>;
-
-                                if (storyName.isEmpty) {
-                                  return Container();
-                                }
-
-                                if (data['storyName']
-                                    .toString()
-                                    .trim()
-                                    .toLowerCase()
-                                    .startsWith(
-                                        storyName.trim().toLowerCase())) {
-                                  return ListTile(
-                                    dense: true,
-                                    //TODO: add support for tapping
-                                    leading: Image.network(data['art']),
-                                    title: Text(
-                                      data['storyName'],
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                    subtitle: Text(
-                                      data['description'],
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                    onTap: () {
-                                      //TODO: pass is name and information?
-                                      Navigator.push(context, MaterialPageRoute(builder: (context) => DescriptionStoryPage(
-                                        art: data['art'],
-                                        authorID: data['authorID'],
-                                        description: data['description'],
-                                        downloadURL: data['downloadURL'],
-                                        series: 'false',
-                                        seriesID: data['seriesID'],
-                                        storyID: data['storyID'],
-                                        storyName: data['storyName'],
-                                        tags: data['tags'],
-                                      )));
-                                    },
-                                  );
-                                }
-                                return Container();
-                              },
-                            );
-                    },
-                  ),
-                ),
-                if (searchController.text.isEmpty) const ShowGenres(), //TODO: add other page things
-              ],
+              ], //children
             ),
-          ),
-        ));
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
+                controller: searchController,
+                decoration: const InputDecoration(
+                    labelText: 'Search for Authors, Stories, Playlist',
+                    prefixIcon: Icon(Icons.search)),
+                onChanged: (val) {
+                  setState(() {
+                    storyName = val;
+                  });
+                },
+              ),
+            ),
+
+            (searchController.text.isEmpty)
+                ? ShowGenres()
+                : SizedBox(
+                    height: 200,
+                    child: StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('Stories')
+                          .snapshots(),
+                      builder: (context, snapshots) {
+                        return (snapshots.connectionState ==
+                                ConnectionState.waiting)
+                            ? const Center(
+                                child: CircularProgressIndicator(),
+                              )
+                            : ListView.builder(
+                                itemCount: snapshots.data!.docs.length,
+                                itemBuilder: (context, index) {
+                                  var data = snapshots.data!.docs[index].data()
+                                      as Map<String, dynamic>;
+
+                                  if (storyName.isEmpty) {
+                                    return Container();
+                                  }
+
+                                  if (data['storyName']
+                                      .toString()
+                                      .trim()
+                                      .toLowerCase()
+                                      .startsWith(
+                                          storyName.trim().toLowerCase())) {
+                                    return ListTile(
+                                      dense: true,
+                                      //TODO: add support for tapping
+                                      leading: Image.network(data['art']),
+                                      title: Text(
+                                        data['storyName'],
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                      subtitle: Text(
+                                        data['description'],
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                      onTap: () {
+                                        //TODO: pass is name and information?
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    DescriptionStoryPage(
+                                                      art: data['art'],
+                                                      authorID:
+                                                          data['authorID'],
+                                                      description:
+                                                          data['description'],
+                                                      downloadURL:
+                                                          data['downloadURL'],
+                                                      series: 'false',
+                                                      seriesID:
+                                                          data['seriesID'],
+                                                      storyID: data['storyID'],
+                                                      storyName:
+                                                          data['storyName'],
+                                                      tags: data['tags'],
+                                                    )));
+                                      },
+                                    );
+                                  }
+                                  return Container();
+                                },
+                              );
+                      },
+                    ),
+                  ), //TODO: add other page things
+          ],
+        ),
+      ),
+    ));
   }
 }
 
@@ -161,12 +171,12 @@ class _ShowGenresState extends State<ShowGenres> {
               Container(
                 padding: const EdgeInsets.all(16),
                 child: const Text(
-                    "Discover",
+                  "Discover",
                   style: TextStyle(
                     fontSize: 22.0,
                     fontWeight: FontWeight.normal,
                     color: Colors.black,
-                   ),
+                  ),
                 ),
               ),
               Container(
@@ -179,7 +189,27 @@ class _ShowGenresState extends State<ShowGenres> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               genreCard('Adventure', 'adventure'),
-              genreCard('Action','action'),
+              genreCard('Action', 'action'),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              genreCard('Mystery ', 'mystery'),
+              genreCard('Thriller', 'thriller'),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              genreCard('Romance', 'romance'),
+              genreCard('Humor', 'humor'),
             ],
           ),
 
@@ -190,17 +220,52 @@ class _ShowGenresState extends State<ShowGenres> {
   }
 
   Widget genreCard(String title, String genre) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      color: Colors.grey,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(title),
-          Image.asset('images/NoImageDefault.jpg',width: 50,)
-        ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ShowGenrePage()));
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        color: Colors.grey,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title),
+            const SizedBox(width: 15),
+            Image.asset(
+              'images/NoImageDefault.jpg',
+              width: 50,
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
+class ShowGenrePage extends StatefulWidget {
+  const ShowGenrePage({Key? key}) : super(key: key);
+
+  @override
+  State<ShowGenrePage> createState() => _ShowGenrePageState();
+}
+
+class _ShowGenrePageState extends State<ShowGenrePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Hello'),
+          ],
+        ),
+      ),
+    );
+  }
+}
